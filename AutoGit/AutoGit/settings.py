4e7a4a32c052554aac6d2a95f81d17a4d199a737
$EN <65331944+gsk-21@users.noreply.github.com>
2021-10-17 20:55:00 +0530

    Update settings.py

--- AutoGit/AutoGit/settings.py
+++ AutoGit/AutoGit/settings.py
@@ -25,7 +25,7 @@ STATIC_DIR = os.path.join(BASE_DIR, 'static')
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-t+@poyn!kt*@pkn-@55!yluq+2%dqrq*o=uap94h+rcs68lqjl'
+SECRET_KEY = '#############'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
